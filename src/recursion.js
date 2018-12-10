426c58d7fc6ee00dfb8cd1a329114ba13f89e920
khengschew <khengc@gmail.com>
2018-12-09 19:11:25 -0800

    Updated minimizeZeroes function

--- src/recursion.js
+++ src/recursion.js
@@ -500,6 +500,15 @@ var augmentElements = function(array, aug) {
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
 var minimizeZeroes = function(array) {
+  if (array.length === 0) {
+    return [];
+  } else if (array.length === 1) {
+    return [array[0]];
+  } else if (array[0] === array[1] && array[0] === 0) {
+    return [].concat(minimizeZeroes(array.slice(1, array.length)));
+  } else {
+    return [array[0]].concat(minimizeZeroes(array.slice(1, array.length)));
+  }
 };
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
