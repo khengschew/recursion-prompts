39ff614b88a0506227890576bc8468cf7e3d2f02
khengschew <khengc@gmail.com>
2018-12-09 18:53:28 -0800

    Updated compress function

--- src/recursion.js
+++ src/recursion.js
@@ -472,6 +472,15 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+  if (list.length === 0) {
+    return [];
+  } else if (list.length === 1) {
+    return [list[0]];
+  } else if (list[0] === list[1]) {
+    return [].concat(compress(list.slice(1, list.length)));
+  } else {
+    return [list[0]].concat(compress(list.slice(1, list.length)));
+  }
 };
 
 // 33. Augument every element in a list with a new value where each element is an array
